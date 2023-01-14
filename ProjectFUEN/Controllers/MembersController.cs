bf3a088e8514c4bc9404777ca0584ec2aab1fdf1
jim2764 <jim2345678@gmail.com>
2023-01-14 20:23:48 +0800

    Member Init
    建立Member Controller

---
+++ ProjectFUEN/Controllers/MembersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectFUEN.Models.EFModels;
+
+namespace ProjectFUEN.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly ProjectFUENContext _context;
+
+        public MembersController(ProjectFUENContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Members
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Members.ToListAsync());
+        }
+    }
+}

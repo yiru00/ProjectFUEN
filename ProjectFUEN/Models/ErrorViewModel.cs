4ca6608519508cf87c7d850aa4dca13aae616d15
mumumu7777 <115806157+mumumu7777@users.noreply.github.com>
2023-01-18 01:15:43 +0800

    Delete ProjectFUEN directory

--- ProjectFUEN/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjectFUEN.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-

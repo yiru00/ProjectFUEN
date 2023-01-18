8e0f309d8983c84fe7112e61de82c74aa67f384c
Yoo <kevin3571630@gmail.com>
2023-01-18 11:43:20 +0800

    new Project

---
+++ ProjectFUEN/Models/DTOs/OrderItemsDTO.cs
@@ -0,0 +1,17 @@
+﻿using ProjectFUEN.Models.EFModels;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ProjectFUEN.Models.DTOs
+{
+    public class OrderItemsDTO
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductNumber { get; set; }
+        public virtual OrderDetail Order { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ ProjectFUEN/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProjectFUEN.Models.EFModels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+//連線字串注入
+var ProjectFUENconnectionString = builder.Configuration.GetConnectionString("ProjectFUEN") ?? throw new InvalidOperationException("Connection string 'Northwind' not found.");
+builder.Services.AddDbContext<ProjectFUENContext>(options =>
+	options.UseSqlServer(ProjectFUENconnectionString));
+
+//builder.Services.AddDbContext<ProjectFUENContext>(options =>
+//	options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectFUEN") ?? throw new InvalidOperationException("Connection string 'ProjectFUEN' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+   name: "default",
+    pattern: "{controller=OrderDetails}/{action=Index}/{id?}");
+
+
+app.Run();
+

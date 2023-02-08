2b01118704a4fbc8d301b1e72d3e799de38ac9e3
HuanYu <j22837749@gmail.com>
2023-02-08 13:32:30 +0800

    更改掉 折價會有.00 Edit改用setValues去更改資料

--- ProjectFUEN/Models/Infrastructures/Repositories/CouponRepository.cs
+++ ProjectFUEN/Models/Infrastructures/Repositories/CouponRepository.cs
@@ -39,13 +39,17 @@ namespace ProjectFUEN.Models.Infrastructures.Repositories
         public void Edit(CouponDto dto)
         {
             Coupon data = db.Coupons.Find(dto.Id);
+            data.Id = dto.Id;
+            
+            db.Entry(data).CurrentValues.SetValues(dto.ToCoupon());
 
-            data.Name = dto.Name;
-            data.Code = dto.Code;
-            data.LeastCost = dto.LeastCost;
-            data.Count = dto.Count;
-            data.Discount = dto.Discount;
-  
+            //data.Name = dto.Name;
+            //data.Code = dto.Code;
+            //data.LeastCost = dto.LeastCost;
+            //data.Count = dto.Count;
+            //data.Discount = dto.Discount;
+
+
             db.SaveChangesAsync();
         }
 
